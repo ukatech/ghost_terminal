5acafeb2fd43ae3d91d15d7f307e3d634ef6c17e
steve green <steve02081504@EDEN.fukwold>
2021-06-09 11:21:16 +0800

    Update ghost_terminal.cpp

--- ghost_terminal.cpp
+++ ghost_terminal.cpp
@@ -1,13 +1,11 @@
 #include "..\SSTP_linker\include\SSTP.hpp"
 #include <iostream>
-#include <Windows.h>
 int main(){
 	using namespace SSTP_link_n;
 	using namespace std;
 	SSTP_link_t linker;
-	{
-		auto names =
-		linker.NOTYFY({ { L"Event", L"ShioriEcho.GetName" } }).to_map();
+	if(linker.Has_Event(L"ShioriEcho.GetName")){
+		auto names = linker.NOTYFY({ { L"Event", L"ShioriEcho.GetName" } });
 		wcout << "terminal login\n";
 		if(names.has(L"GhostName"))
 			wcout << "ghost: " << names[L"GhostName"] << '\n';
@@ -19,25 +17,31 @@ int main(){
 		SSTP_link_args_t Result={};
 		wcout << ">> ";
 		while (wcin >> commad && commad != L"exit") {
+			if(!linker.Has_Event(L"ShioriEcho"))
+				wcout << "Event Has_Event or ShioriEcho Not define.\n";
 			linker.NOTYFY({ { L"Event", L"ShioriEcho" },
 							{ L"Reference0", commad }
 						  });
-			do{
-				Result=linker.NOTYFY({ { L"Event", L"ShioriEcho.GetResult" } }).to_map();
-				if(Result.has(L"Special")){
-					wcout << Result[L"Special"] << endl;
-					break;
-				}else if(Result.has(L"Result")){
-					wcout << Result[L"Result"] << endl;
-					if(Result.has(L"Type"))
-						wcout << L"Type: " << Result[L"Type"] << endl;
-					break;
-				}
-				else{
-					Sleep(1000);
-					continue;
-				}
-			}while(1);
+			if(linker.Has_Event(L"ShioriEcho.GetResult")){
+				do{
+					Result=linker.NOTYFY({ { L"Event", L"ShioriEcho.GetResult" } });
+					if(Result.has(L"Special")){
+						wcout << Result[L"Special"] << endl;
+						break;
+					}else if(Result.has(L"Result")){
+						wcout << Result[L"Result"] << endl;
+						if(Result.has(L"Type"))
+							wcout << "Type: " << Result[L"Type"] << endl;
+						break;
+					}
+					else{
+						Sleep(1000);
+						continue;
+					}
+				}while(1);
+			}
+			else
+				wcout << "Event Has_Event or ShioriEcho.GetResult Not define.\n";
 			wcout << ">> ";
 		}
 	}
