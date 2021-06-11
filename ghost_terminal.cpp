429c3f5e8c9ecedb0aaee3626cc6a501ead12c97
steve green <steve02081504@EDEN.fukwold>
2021-06-11 12:04:35 +0800

    Update ghost_terminal.cpp

--- ghost_terminal.cpp
+++ ghost_terminal.cpp
@@ -1,16 +1,10 @@
 #include "..\SSTP_linker\include\SSTP.hpp"
 #include <iostream>
-#ifdef _WIN32
-	#include <fcntl.h>
-	#include <io.h>
-#endif
 int main(){
 	using namespace SSTP_link_n;
 	using namespace std;
-	#ifdef _WIN32
-		_setmode(_fileno(stdout), _O_U16TEXT);
-		_setmode(_fileno(stdin), _O_U16TEXT);
-	#endif
+	wcin.imbue(locale(""));
+	wcout.imbue(locale(""));
 	SSTP_link_t linker({{L"Charset",L"UTF-8"},{L"Sender",L"Ghost Terminal"}});
 	{
 		auto names = linker.NOTYFY({ { L"Event", L"ShioriEcho.GetName" } });
